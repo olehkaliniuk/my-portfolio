3f3992db183d6ac44d71de534521fe85db25b8a3
Oleh Kaliniuk <>
2025-02-04 00:16:51 +0100

    first commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,36 @@
-import React from 'react'
-import "./Header.css"
-import { NavLink } from 'react-router-dom'
-
-import "../../ i18n.js"
+import React, { useState } from 'react';
+import "./Header.css";
+import { NavLink } from 'react-router-dom';
+import menu from "../../img/menu.png";
 import { useTranslation } from "react-i18next";
 
 function Header() {
-
     const { t, i18n } = useTranslation();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     const toggleLanguage = () => {
         const newLang = i18n.language === "en" ? "de" : "en";
         i18n.changeLanguage(newLang);
     };
 
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div className='navcontainer'>
             <div className="gradient-border" id="box">
+                
                 <ul>
-
-                    <li><NavLink className="navl" to="/">{t("Home")}</NavLink></li>
-                    <li><NavLink className="navl" to="/projects">{t("Projects")}</NavLink></li>
-                    <li><NavLink className="navl" to="/contacts">{t("Contacts")}</NavLink></li>
-                    <li><button className='languagebtn' onClick={toggleLanguage}>{t("button")}</button></li>
+                    <button className='menubtn' onClick={toggleMenu}> <img src={menu} alt="" /></button>
+                    <li className={isMenuOpen ? "open" : "close"}><NavLink className="navl" onClick={toggleMenu} to="/" >{t("Home")}</NavLink></li>
+                    <li className={isMenuOpen ? "open" : "close"}><NavLink className="navl" onClick={toggleMenu} to="/projects" >{t("Projects")}</NavLink></li>
+                    <li className={isMenuOpen ? "open" : "close"}><NavLink className="navl" onClick={toggleMenu} to="/contacts" >{t("Contacts")}</NavLink></li>
+                    <li className={isMenuOpen ? "open" : "close"}><button className="languagebtn"  onClick={toggleLanguage}>{t("button")}</button></li>
                 </ul>
             </div>
         </div>
-
-    )
+    );
 }
 
-export default Header
+export default Header;
